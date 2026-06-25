203a14ba85c23a1838a4a0767d962fbec3431c1d
martykribs-alt <marty.kribs@gmail.com>
2026-06-25 14:08:23 -0700

    Fix: remove broken sitemap integration to unblock Netlify build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://acre.homes',
-  integrations: [sitemap()],
   build: {
     inlineStylesheets: 'auto',
   },
